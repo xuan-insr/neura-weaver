b506f3b67d97e133a5db4d6dea02b23017dffd48
NeuraWeaverAutoSync <xianyuxuan@outlook.com>
2025-01-31 12:06:31 +0000

    Sync from private repo

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "**/private"],
+    ignorePatterns: ["templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
